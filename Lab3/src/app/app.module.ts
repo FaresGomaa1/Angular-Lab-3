47e134ba7897e528cd9caf22b75bc2184d0ba877
FaresGomaa1 <faresgomaa336@gmail.com>
2024-03-01 03:51:29 +0200

    upload

--- Lab3/src/app/app.module.ts
+++ Lab3/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ProductComponent } from './parent/product/product.component';
+import { TabelComponent } from './parent/tabel/tabel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ProductComponent,
+    TabelComponent
   ],
   imports: [
     BrowserModule
